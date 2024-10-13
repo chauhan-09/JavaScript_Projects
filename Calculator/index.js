364499c9f0054d4e9ee9d2cdf2ac729529a1b902
narend99 <nsc7574@gmail.com>
2024-10-13 21:17:51 +0530

    made changes

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,22 +1,28 @@
-const screen = document.querySelector('#screen');
 const btns = document.querySelectorAll('.btn');
 const btn_equal = document.querySelector('.btn_equal');
 const btn_clear = document.querySelector('.btn_clear');
+const screen = document.querySelector('.screen');
 
 for(let i=0;i<btns.length;i++)
 {
-    btns[i].addEventListener('click',function (){
-        let num = btns[i].getAttribute('data-num');
-        screen.value += num;
+    let number = "";
+    btns[i].addEventListener('click',function()
+    {
+        number = btns[i].getAttribute('data-num');
+        screen.value += number;
     })
+
 }
 
-btn_equal.addEventListener('click',function(){
-    let ans = eval(screen.value);
-    screen.value = ans;
+btn_equal.addEventListener('click',function()
+{
+    if(screen.value === "") alert('no values entered');
+    else screen.value = eval(screen.value);
 })
 
-btn_clear.addEventListener('click',function(){
-    if(screen.value === "") alert('no value entered');
-    else screen.value = "";
+btn_clear.addEventListener('click',function()
+{
+    screen.value = "";
 })
+
+
