58fdd942a9cd18692b1dd3767184b412cc9d15e4
chauhan-09 <nsc7574@gmail.com>
2024-10-16 19:18:02 +0530

    made changes

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,31 +1,28 @@
+const screen = document.querySelector('.screen');
 const btns = document.querySelectorAll('.btn');
 const btn_equal = document.querySelector('.btn_equal');
 const btn_clear = document.querySelector('.btn_clear');
-const screen = document.querySelector('.screen');
 
 for(let i=0;i<btns.length;i++)
 {
-    let number = "";
-    btns[i].addEventListener('click',function()
-    {
-        number = btns[i].getAttribute('data-num');
+    btns[i].addEventListener('click', function() {
+        let number = btns[i].getAttribute('data-num');
         screen.value += number;
-    })
-
+    }
+    )
 }
 
-btn_equal.addEventListener('click',function()
-{
-    if(screen.value === "") alert('no values entered');
+btn_equal.addEventListener('click',function(){
+    if(screen.value === "") alert("no values entered");
     else screen.value = eval(screen.value);
 })
 
-btn_clear.addEventListener('click',function()
-{
+btn_clear.addEventListener('click',function(){
     screen.value = "";
 })
 
-//Calculator
+
+
 
 
 
---
+++ Digital Clock/index.js
@@ -0,0 +1,28 @@
+const clock = document.querySelector('.clock');
+function showTime()
+{
+    const time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let zone = "AM";
+    if(hours == 0) hours = 12;
+    else if(hours == 12) zone = "PM";
+    if(hours > 12)
+    {
+        hours = hours % 12;
+        zone = "PM";
+    }
+
+    if(hours < 10) hours = '0' + hours;
+    if(minutes < 10) minutes = '0' + minutes;
+    if(seconds < 10) seconds = '0' + seconds;
+
+
+    clock.innerHTML = `${hours} : ${minutes} : ${seconds} ${zone}`;
+    
+}
+
+
+showTime();
+setInterval(showTime,1000);
